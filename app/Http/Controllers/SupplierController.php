107f37a36a31073c6c3309aaa18af268290b6696
ramos013 <334020@student.mboutrecht.nl>
2024-06-26 10:18:14 +0200

    setting up project and all needed home page supplier page and routes

---
+++ app/Http/Controllers/SupplierController.php
@@ -0,0 +1,16 @@
+<?php
+
+// app/Http/Controllers/SupplierController.php
+namespace App\Http\Controllers;
+
+use App\Models\Supplier;
+use Illuminate\Http\Request;
+
+class SupplierController extends Controller
+{
+    public function index()
+    {
+        $suppliers = Supplier::all();
+        return view('suppliers.suppliers', compact('suppliers'));
+    }
+}
