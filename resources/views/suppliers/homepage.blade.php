90757fdf1b9e0de2da0c3094a4ab3309264cb437
ramos013 <334020@student.mboutrecht.nl>
2024-06-27 09:28:13 +0200

    added link

--- resources/views/suppliers/homepage.blade.php
+++ resources/views/suppliers/homepage.blade.php
@@ -13,7 +13,6 @@
         <h1 class="text-center">Homepage voedselbank maaskantje</h1>
         <div class="text-center">
             <a href="{{ route('suppliers.index') }}" class="btn btn-primary">Overzicht Leveranciers</a>
-            <!-- Example link to show products -->
             <a href="{{ route('suppliers.showProducts', ['id' => 1]) }}" class="btn btn-secondary">Overzicht Producten</a>
         </div>
     </div>
